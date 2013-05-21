c8a7bd96ec2cff0a52cd453ea94c2bd65535d4ce
olivier.nouguier <olivier.nouguier@gmail.com>
2013-05-21 23:40:34 +0200

    First working macro

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ name := "play-crude-macros"
 version := "0.0.1-SNAPSHOT"
 
 libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _)
+
+libraryDependencies += "play" %% "play" % "2.1.0"
--- src/main/scala/play/crude/insert.scala
+++ src/main/scala/play/crude/insert.scala
@@ -1,16 +1,25 @@
 package play.crude
 
 import language.experimental.macros
-
 import reflect.macros.Context
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import play.api.libs.json.JsResult
+import play.api.mvc.SimpleResult
+
+trait Cruded[A] {
+  def insert(a: A) : Long
+}
 
 object CrudeMacro {
 
-  def insert[A](o: A) = macro insert_impl[A]
   
-  def insert_impl[A](c: Context)(o: c.Expr[A])  = {
-   import c.universe._
-    val str = o.tree
-    reify { println( " zoz "+  o.splice) }
+  
+  def insert[A](json: JsValue, v: JsValue => JsResult[A], cruded: Cruded[A], j: A => SimpleResult[JsValue]) = macro insert_impl[A]
+
+  def insert_impl[A](c: Context)(json: c.Expr[JsValue], v: c.Expr[JsValue => JsResult[A]] , cruded: c.Expr[Cruded[A]], j: c.Expr[A => SimpleResult[JsValue]]) = {
+    import c.universe._
+    val str = json.tree
+    reify { v.splice(json.splice).map {o => cruded.splice.insert( o ); j.splice(o)} }
   }
 }
